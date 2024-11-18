c0a387280c19e3c1591b5700ef2160b137e5daec
riccardocadei <riccardo.cadei98@gmail.com>
2024-11-18 14:50:10 +0100

    add compute effect

--- src/models.py
+++ src/models.py
@@ -1,5 +1,11 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+
+from econml.dr import DRLearner
+from econml.dml import LinearDML
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 
 class MLP(nn.Module):
     '''
@@ -40,4 +46,34 @@ class ConvNet(nn.Module):
         x = F.relu(self.fc1(x))
         logits = self.fc2(x).flatten()
         return logits
-    
+
+def compute_effect(dataset, method="AD", pred=False):
+    if pred:
+        Y = dataset.Y_hat.numpy()
+    else:
+        Y = dataset.Y.numpy()
+    T = dataset.T.numpy()
+    W = dataset.W.numpy()
+    U = dataset.U.numpy()
+
+    if method == "AD":
+        return np.mean(Y[T == 1]) - np.mean(Y[T == 0])
+    if method == "AF":
+        return np.mean(Y[(T == 1) & (W == 1)])*np.mean(W) + \
+               np.mean(Y[(T == 1) & (W == 0)])*(1 - np.mean(W)) - \
+               np.mean(Y[(T == 0) & (W == 1)])*np.mean(W) - \
+               np.mean(Y[(T == 0) & (W == 0)])*(1 - np.mean(W))
+    if method == "AIPW":
+        # TODO: understand why not better than AF
+        model = LinearDML(model_t=LogisticRegression(), 
+                          model_y=RandomForestClassifier(),
+                          discrete_treatment=True,
+                          random_state=1)
+        if len(W.shape) == 1:
+            W = np.expand_dims(W, axis=1)
+        if len(U.shape) == 1:
+            U = np.expand_dims(U, axis=1)
+
+        X = np.concatenate((W, U), axis=1)
+        model.fit(Y=Y, T=T, X=X); 
+        return model.ate(X=X)
