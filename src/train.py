c6e09acf6510ecbf76846a17a773edbd8f16fe9b
riccardocadei <riccardo.cadei98@gmail.com>
2024-11-27 15:01:00 +0100

    update train function

--- src/train.py
+++ src/train.py
@@ -3,91 +3,7 @@ import torch.nn.functional as F
 from torchvision import transforms
 import torch.optim as optim
 from sklearn.metrics import balanced_accuracy_score, accuracy_score
-
-from dataset import CausalMNIST
-from models import ConvNet
-
-def performances_val(model, data_loader, verbose=False, device = 'cpu'):
-    model.eval()
-    with torch.no_grad():
-        images = torch.stack([image for image, _ in data_loader.dataset])
-        output = model(images)
-        y_pred_probs = torch.sigmoid(output)
-        y_pred_bin = torch.where(torch.gt(output, torch.Tensor([0.0]).to(device)),
-                             torch.Tensor([1.0]).to(device),
-                             torch.Tensor([0.0]).to(device))  
-        labels = torch.tensor([label for _, label in data_loader.dataset])
-        y = labels[:,3].float()
-        t = labels[:,0]
-        ead = torch.mean(y[t==1]) - torch.mean(y[t==0])
-        ead_prob = torch.mean(y_pred_probs[t==1]) - torch.mean(y_pred_probs[t==0])
-        #ead_binary = torch.mean(y_pred_bin[t==1]) - torch.mean(y_pred_bin[t==0])
-        TEB = ead_prob - ead
-        loss = F.binary_cross_entropy_with_logits(output, y)
-        acc = accuracy_score(y, y_pred_bin)
-        bacc = balanced_accuracy_score(y, y_pred_bin)
-        
-        if verbose:
-            print(f'Validation set: Average loss: {loss.item():.4f}, Accuracy: {acc:.2f}, Balanced Accuracy: {bacc:.2f}, TEB: {TEB:.2f}')
-    return loss.item(), acc.item(), bacc.item(), TEB.item()
-
-def performances_all(model, data_loader, verbose=False, device = 'cpu'):  
-    model.eval()
-    with torch.no_grad():
-        images = torch.stack([image for image, _ in data_loader.dataset])
-        output = model(images)
-        y_pred_probs = torch.sigmoid(output)
-        y_pred_bin = torch.where(torch.gt(output, torch.Tensor([0.0]).to(device)),
-                             torch.Tensor([1.0]).to(device),
-                             torch.Tensor([0.0]).to(device))  
-        labels = torch.tensor([label for _, label in data_loader.dataset])
-        y = labels[:,3].float()
-        t = labels[:,0]
-        ead = torch.mean(y[t==1]) - torch.mean(y[t==0])
-        ead_prob = torch.mean(y_pred_probs[t==1]) - torch.mean(y_pred_probs[t==0])
-        ead_binary = torch.mean(y_pred_bin[t==1]) - torch.mean(y_pred_bin[t==0])
-        ATE = 0.3
-        TEB = ead_prob - ATE
-        TEB_bin = ead_binary - ATE
-        acc = accuracy_score(y, y_pred_bin)
-        bacc = balanced_accuracy_score(y, y_pred_bin)
-        
-        if verbose:
-            print(f'All set: Accuracy: {acc:.2f}, Balanced Accuracy: {bacc:.2f}, TEB: {TEB:.2f}, TEB_bin: {TEB_bin:.2f}, EAD: {ead:.2f}')
-    return acc.item(), bacc.item(), TEB.item(), TEB_bin.item(), ead.item()
-
-
-def compute_ead(model, data_loader, device = 'cpu'):
-    '''
-    Compute Empirical Associational Difference (EAD) for the given 
-    model and data_loader.
-
-    Args:
-        model: torch.nn.Module
-        data_loader: torch.utils.data.DataLoader
-        device: str
-    '''
-    model.eval()
-    y = torch.tensor([])
-    b = torch.tensor([])
-    y_prob = torch.tensor([])
-    y_binary = torch.tensor([])
-    with torch.no_grad():
-        for data, target in data_loader:
-            X_b, y_b = data.to(device), target[3].to(device).float()
-            b_b = target[0].to(device).float()
-            y_prob_b = torch.sigmoid(model(X_b))
-            y_binary_b = torch.where(y_prob_b > 0.5, 
-                                 torch.tensor(1.0).to(device), 
-                                 torch.tensor(0.0).to(device))
-            y = torch.cat((y, y_b), 0)
-            b = torch.cat((b, b_b), 0)
-            y_prob = torch.cat((y_prob, y_prob_b), 0)
-            y_binary = torch.cat((y_binary, y_binary_b), 0)
-        ead = torch.mean(y[b==1]) - torch.mean(y[b==0])
-        ead_prob = torch.mean(y_prob[b==1]) - torch.mean(y_prob[b==0])
-        ead_binary = torch.mean(y_binary[b==1]) - torch.mean(y_binary[b==0])
-    return ead, ead_prob, ead_binary
+from models import compute_effect
 
 def training(model,
              dataset, 
@@ -114,8 +30,9 @@ def training(model,
 
     model = model.to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    train = dataset.data_label_tuples[:int(train_ratio*len(dataset))]
-    val = dataset.data_label_tuples[int(train_ratio*len(dataset)):]
+    n_tr = int(train_ratio*len(dataset))
+    train = dataset.data_label_tuples[:n_tr]
+    #val = dataset.data_label_tuples[n_tr:]
     train_loader = torch.utils.data.DataLoader(train, 
                                                batch_size=batch_size, 
                                                shuffle=True, 
@@ -128,60 +45,33 @@ def training(model,
     model.train()
     for epoch in range(epochs):
         for batch_idx, (image, variables) in enumerate(train_loader):
-            X, y = image.to(device), variables[3].to(device).float()
+            X, y = image.to(device).float(), variables[3].to(device).long()
             optimizer.zero_grad()
             output = model(X)
-            # TODO: check loss
-            loss = F.binary_cross_entropy_with_logits(output, y)
+            loss = torch.nn.CrossEntropyLoss()(output, y)
             loss.backward()
             optimizer.step()
             if batch_idx % 100 == 0 and verbose:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(X), len(train_loader.dataset),
                         100. * batch_idx / len(train_loader), loss.item()))
-        # evaluate(model, device, train_loader, verbose, set_name='train')
-        # evaluate(model, device, val_loader, verbose, set_name='val')
+        
+        # measure accuracy
+        model.eval()
+        with torch.no_grad():
+            dataset.Y_hat = model(dataset.X).max(axis=1)[1].numpy()
+            tr_acc = accuracy_score(dataset.Y[:n_tr], dataset.Y_hat[:n_tr])
+            tr_bal_acc = balanced_accuracy_score(dataset.Y[:n_tr], dataset.Y_hat[:n_tr])
+            val_acc = accuracy_score(dataset.Y[n_tr:], dataset.Y_hat[n_tr:])
+            val_bal_acc = balanced_accuracy_score(dataset.Y[n_tr:], dataset.Y_hat[n_tr:])
+            print(f'Train accuracy: {tr_acc:.2f}, Train balanced accuracy: {tr_bal_acc:.2f}')
+            print(f'Validation accuracy: {val_acc:.2f}, Validation balanced accuracy: {val_bal_acc:.2f}')
+            AD_ = compute_effect(dataset, method="AD", pred=True)
+            AF_ = compute_effect(dataset, method="AF", pred=True)
+            print(f'Pred. AD: {AD_:.2f}, Pred. AF: {AF_:.2f}')
 
     # TODO: return best model
     return model   
-
-def evaluate(model, device, loader, verbose, set_name=""):
-    '''
-    Evaluate the model on the given dataset.
-
-    Args:
-        model: torch.nn.Module
-        device: str
-        loader: torch.utils.data.DataLoader
-        verbose: bool
-        set_name: str
-    '''
-    model.eval()
-
-    # get Acc, Balanced Acc, AD, AF
-    # get Y_hat
-
-
-    loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in loader:
-            data, target = data.to(device), target[3].to(device).float()
-            output = model(data)
-            loss += F.binary_cross_entropy_with_logits(output, target, reduction='sum').item() 
-            pred = torch.where(torch.gt(output, torch.Tensor([0.0]).to(device)),
-                               torch.Tensor([1.0]).to(device),
-                               torch.Tensor([0.0]).to(device))  
-            correct += pred.eq(target.view_as(pred)).sum().item()
-
-    loss /= len(loader.dataset)
-    if verbose:
-        print('Performance on {}: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
-            set_name, loss, correct, len(loader.dataset),
-            100. * correct / len(loader.dataset)))
-
-    return 100. * correct / len(loader.dataset)
-
 
 
 
